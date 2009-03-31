2dc3486d40fe95233b1ab3e3014e6cf62343f3ba
Adam Goforth <tennisbike@hotmail.com>
2009-03-31 03:49:07 +0000

    Fix the unit tests.

--- cutimagestest.py
+++ cutimagestest.py
@@ -32,27 +32,27 @@ class CutImage(unittest.TestCase):
 
 		width, height = dict["topleft"].size
 		self.assertEqual(width, 244)
-		self.assertEqual(height, 242)
+		self.assertEqual(height, 243)
 
 		width, height = dict["topmiddle"].size
 		self.assertEqual(width, 129)
-		self.assertEqual(height, 242)
+		self.assertEqual(height, 243)
 
 		width, height = dict["topright"].size
 		self.assertEqual(width, 362)
-		self.assertEqual(height, 242)
+		self.assertEqual(height, 243)
 
 		width, height = dict["bottomleft"].size
 		self.assertEqual(width, 194)
-		self.assertEqual(height, 245)
+		self.assertEqual(height, 244)
 
 		width, height = dict["bottommiddle"].size
 		self.assertEqual(width, 298)
-		self.assertEqual(height, 245)
+		self.assertEqual(height, 244)
 
 		width, height = dict["bottomright"].size
 		self.assertEqual(width, 243)
-		self.assertEqual(height, 245)
+		self.assertEqual(height, 244)
 
 	def testDumpPanels(self):
 		testImage = Image.open("test/comic.png")
@@ -62,7 +62,7 @@ class CutImage(unittest.TestCase):
 		tlpanelImage = Image.open("test/topleft/comic-topleft.png")
 		width, height = tlpanelImage.size
 		self.assertEqual(width, 244)
-		self.assertEqual(height, 242)
+		self.assertEqual(height, 243)
 
 if __name__ == "__main__":
 	unittest.main()
