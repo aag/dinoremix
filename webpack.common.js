6b5e236b603f1ac54dd6f652ae660dfbe3aacfe6
Adam Goforth <code@adamgoforth.com>
2019-02-20 12:56:40 -0500

    Add Babel back in
    The .babelrc and .browserslistrc files still existed from before, so we
    just need to add the dependency and Webpack rule.

--- webpack.common.js
+++ webpack.common.js
@@ -42,6 +42,13 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
       }
     ]
   }
