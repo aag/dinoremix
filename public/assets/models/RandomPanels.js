38aa08b0fcd8d765b0d91362c16f853183b89649
Adam Goforth <code@adamgoforth.com>
2019-02-19 23:06:53 -0500

    Preload the next panels
    Use javascript to make the browser start downloading the next set of
    panels as soon as their URLs are known. This should result in the
    ReloadButton displaying the new panels immediately for almost all users.

--- public/assets/models/RandomPanels.js
+++ public/assets/models/RandomPanels.js
@@ -1,11 +1,15 @@
 const m = require('mithril');
 
+const Url = require('../helpers/Url');
+
 const RandomPanels = {
   panels: {},
+  images: [],
 
   loadFromGlobal: () => {
     if (window.dr && window.dr.nextPanels) {
       RandomPanels.panels = window.dr.nextPanels;
+      RandomPanels.preloadImages();
     }
   },
 
@@ -17,7 +21,19 @@ const RandomPanels = {
       result.forEach((panel) => {
         RandomPanels.panels[panel.pos] = panel.id;
       });
+
+      RandomPanels.preloadImages();
     }),
+
+  preloadImages: () => {
+    RandomPanels.images = [];
+
+    Object.entries(RandomPanels.panels).forEach(([pos, id]) => {
+      const image = new Image();
+      image.src = Url.getImageUrl(pos, id);
+      RandomPanels.images.push(image);
+    });
+  },
 };
 
 module.exports = RandomPanels;
