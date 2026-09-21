52987260ea22304212c217d6137108858df84725
Adam Goforth <code@adamgoforth.com>
2026-09-20 20:20:26 -0400

    Update Deployer and its config
    Update the vendored deployer.phar. Also update the config to restart the
    current version of PHP FPM that's running on the server.

--- deploy.php
+++ deploy.php
@@ -52,7 +52,7 @@ task('deploy:npm_build', function () {
 })->desc('Build frontend assets');
 
 task('deploy:fpm_restart', function () {
-    $output = run('sudo service php7.3-fpm restart');
+    $output = run('sudo service php8.4-fpm restart');
 })->desc('Restart PHP-FPM');
 
 desc('Deploy Dinosaur Remix');
